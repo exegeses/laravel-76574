8335f6788ce82eb85c563da89eac4eb19d799d27
Marcos <exegeses@gmail.com>
2025-09-08 20:32:40 -0300

    seeder de productos

--- catalogo/database/seeders/CategoriaSeeder.php
+++ catalogo/database/seeders/CategoriaSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Categoria;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,15 @@ class CategoriaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Categoria::insert(
+            [
+                [ 'catNombre'=>'Smartphone' ],
+                [ 'catNombre'=>'Parlantes Bluetooth' ],
+                [ 'catNombre'=>'Robot de limpieza' ],
+                [ 'catNombre'=>'Smart TV' ],
+                [ 'catNombre'=>'Cámaras Mirrorless' ],
+                [ 'catNombre'=>'Iluminación inteligente' ],
+            ]
+        );
     }
 }
